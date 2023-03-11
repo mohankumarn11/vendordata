c3d9b1c54281a3c4f5a002ed5e8cf998da46ac50
mohankumarn11 <mohan.kumar.11@outlook.com>
2023-03-11 09:41:46 +0000

    initial commit

---
+++ src/test/java/ai/cloudeagle/vendordata/VendordataApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.cloudeagle.vendordata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendordataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
